13661aeca98edb6aed68e218e671e63d45b1bac4
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-03-17 15:18:15 +0200

    fix style

--- src/controllers/getProfilepage.js
+++ src/controllers/getProfilepage.js
@@ -1,17 +1,8 @@
 const { join } = require('path');
 
 const getProfilePage = (req, res) => {
-//   const cookies = req.cookies.token;
+  if (!req.id) res.redirect('/login');
   res.sendFile(join(__dirname, '..', '..', 'public', 'html', 'profile.html'));
 };
-// const getProfilePage = (req, res) => {
-//     const cookies = req.cookies.token;
-//     verify(cookies, process.env.SECRET_KEY, (err, decoded) => {
-//       if (err) res.json({ msg: 'Un Authorized' });
-//       else {
-//         res.sendFile(join(__dirname, '..', '..', 'public', 'html', 'profile.html'));
-//       }
-//     });
-//   };
 
 module.exports = getProfilePage;
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,7 @@
 const { join } = require('path');
 
 const homeHandle = (req, res) => {
+  if (!req.id) res.redirect('/login');
   res.sendFile(join(__dirname, '..', '..', 'public', 'html', 'home.html'));
 };
 
